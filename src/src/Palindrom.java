85ccf56605d7cdf41bf33e882f5913fedab7ae41
Kyryl Boroda <kirilboroda25@gmail.com>
2023-02-28 18:40:40 +0200

    my homework

---
+++ src/src/Palindrom.java
@@ -0,0 +1,19 @@
+public class Palindrom {
+
+    private static boolean  isPalindrome(String s){
+        if(s.length()==1 || s.length() == 0)
+            return true;
+
+        if(s.charAt(0) == s.charAt(s.length() - 1))
+            return isPalindrome(s.substring(1,s.length() - 1));
+
+
+        return false;
+
+    }
+    public static void main(String[] args) {
+String s = "KAYAK";
+        System.out.println(isPalindrome(s) ? "YES":"NO");
+
+    }
+}
